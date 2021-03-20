3444e9d13d01aa67eedd7aac8900fa848ffa451a
디알_한경수 <dire@afreecatv.com>
2021-03-20 12:44:37 +0900

    셜록

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # boost_template
+
+```
+cd /tmp
+wget https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.gz
+tar zxvf boost_1_72_0.tar.gz
+boost_1_72_0
+./bootstrap.sh
+sudo ./b2 install
+
+```
+
+```
+git clone https://github.com/hsnks100/boost_template.git 
+cd boost_template
+mkdir build
+cd build
+cmake ..
+make -j4
+```
+
+안되면 CMakeLists.txt 에 설정된 시스템에 설치된 openssl 경로 확인.
+
+openssl 직접설치(선택)
+```
+cd /tmp 
+wget https://www.openssl.org/source/openssl-1.1.0.tar.gz 
+tar zxvf openssl-1.1.0.tar.gz 
+cd openssl-1.1.0
+./config
+make
+sudo make install
+
+```
+
+
---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/websocket.hpp>
+#include <boost/asio/dispatch.hpp>
+#include <boost/asio/strand.hpp>
+#include <algorithm>
+#include <cstdlib>
+#include <functional>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include <boost/beast/ssl.hpp>
+#include <boost/beast/websocket/ssl.hpp>
+#include <regex>
+#include <locale>
+
+namespace beast = boost::beast;         // from <boost/beast.hpp>
+namespace http = beast::http;           // from <boost/beast/http.hpp>
+namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
+namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace ssl = boost::asio::ssl;       // from <boost/asio/ssl.hpp>
+
+
+int main() {
+    int compileTest=3;
+    net::io_context ioc{compileTest}; 
+    
+}
